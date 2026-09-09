db0d0847d50c666ecab7ad941d41a298983717b6
unknown <adewalesalam09@gmail.com>
2026-09-09 09:30:50 +0100

    commit

--- config/db.js
+++ config/db.js
@@ -4,13 +4,11 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const MONGODB = process.env.MONGODB_URL;
-
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGODB);
     console.log("Database Connected Successfully");
   } catch (error) {
-    console.error("DATABASE CONNECTION ERROR:", error);
     console.error(error);
   }
 };
